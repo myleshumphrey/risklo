85155a83756a2f93d9f7347060a31d9d54931d27
Machi <myles2595@gmail.com>
2025-12-11 13:29:43 -0500

    update

--- client/src/components/Icons.js
+++ client/src/components/Icons.js
@@ -143,3 +143,11 @@ export const IconPro = ({ size = 20, color = 'currentColor' }) => (
   </svg>
 );
 
+export const IconLightbulb = ({ size = 20, color = 'currentColor' }) => (
+  <svg width={size} height={size} viewBox="0 0 24 24" fill="none" stroke={color} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+    <line x1="9" y1="18" x2="15" y2="18"></line>
+    <line x1="10" y1="22" x2="14" y2="22"></line>
+    <path d="M15.09 14c.18-.98.65-1.74 1.41-2.5A4.65 4.65 0 0 0 18 8a6 6 0 0 0-9-5.25A6 6 0 0 0 6 8c0 1 .23 2.23 1.5 3.5A4.65 4.65 0 0 1 9 14"></path>
+  </svg>
+);
+
